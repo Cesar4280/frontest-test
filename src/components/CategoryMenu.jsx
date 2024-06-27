acf7f4c419b63c46c754690194ad0b2d25b8fcee
Cesar Fajardo <cesarjavierfajardoortiz@gmail.com>
2024-06-26 21:36:52 -0500

    Implementando carge de las categorias

--- src/components/CategoryMenu.jsx
+++ src/components/CategoryMenu.jsx
@@ -1,5 +1,52 @@
-const CategoryMenu = () => {
-  return <div>CategoryMenu</div>;
+// src/components/CategoryMenu.jsx
+import React, { useState, useEffect } from "react";
+import { fetchCategories } from "../services/mercadoLibreAPI";
+
+const CategoryMenu = ({ onSelectCategory }) => {
+  const [categories, setCategories] = useState([]);
+  const [expandedCategories, setExpandedCategories] = useState({});
+
+  useEffect(() => {
+    const loadCategories = async () => {
+      const data = await fetchCategories();
+      setCategories(data);
+    };
+    loadCategories();
+  }, []);
+
+  const toggleCategory = (category_id) => {
+    setExpandedCategories((prevState) => ({
+      ...prevState,
+      [category_id]: !prevState[category_id],
+    }));
+  };
+
+  return (
+    <div className="category-menu">
+      {categories.map((category) => (
+        <div key={category.id}>
+          <button onClick={() => toggleCategory(category.id)}>
+            {expandedCategories[category.id] ? "-" : "+"}
+          </button>
+          <span onClick={() => onSelectCategory(category.id)}>
+            {category.name}
+          </span>
+          {expandedCategories[category.id] && (
+            <div className="subcategories">
+              {category.children.map((subcategory) => (
+                <div
+                  key={subcategory.id}
+                  onClick={() => onSelectCategory(subcategory.id)}
+                >
+                  {subcategory.name}
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default CategoryMenu;
